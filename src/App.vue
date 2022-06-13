4cf5a7d47e4e98d5f945693511d68302db1eaa92
ruthrootz <ruthrootz@users.noreply.github.com>
2022-06-13 13:32:24 -0400

    background color

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,16 @@
 </template>
 
 <style>
+body {
+  background-color: #04050D;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #C8D1D8;
 }
 
 #nav {
