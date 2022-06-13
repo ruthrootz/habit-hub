74b4e3e62a6561d8b953da960ebb867001cc2284
ruthrootz <ruthrootz@users.noreply.github.com>
2022-06-13 13:16:28 -0400

    setting up HabitHub component

---
+++ src/components/HabitHub.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HabitHub extends Vue {
+  @Prop() private gridState!: number[][];
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HabitHub />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HabitHub from '@/components/HabitHub.vue';
 
 export default {
-  name: 'Home',
+  name: 'HabitHub',
   components: {
-    HelloWorld
+    HabitHub,
   }
 }
 </script>
