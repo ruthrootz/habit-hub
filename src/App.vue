89f164c81df4923ae3e53a9773e6144d1bd9452c
ruthrootz <ruthrootz@users.noreply.github.com>
2022-07-23 18:18:09 -0400

    changed primary color, added scss file

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,6 @@
   </div>
 </template>
 
-<style>
-body {
-  background-color: #04050D !important;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #C8D1D8;
-  width: 75%;
-  margin: 0 auto;
-}
+<style lang="scss">
+@import '@/assets/custom.scss';
 </style>
